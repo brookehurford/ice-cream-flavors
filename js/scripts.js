ab10c9ea4e938c3f95f381a6e82b15ac35ef1729
Matthew Newell and brooke hurford <student@epicodus.com>
2016-01-13 15:57:21 -0800

    Add the sum of arbritrary numbers

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
     flavors.forEach(function(flavor) {
       $("#flavors").append("<li>" + flavor + "</li>")
     });
+
+    var total = 0;
+    numbers = [1, 23 ,44 ,19];
+
+    numbers.forEach(function(number) {
+      $("#flavors").text(total += number);
+    })
 });
